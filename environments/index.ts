90d08087fa2658c77d619740adb917511a236bb4
bbogeul <illumeweb@gmail.com>
2020-08-31 21:21:32 +0900

    removed env variables
    removed env variables

---
+++ environments/index.ts
@@ -0,0 +1,5 @@
+export * from './development.environment';
+export * from './environment.type';
+// export * from './kakao.environment';
+export * from './prod.environment';
+export * from './staging.environment';

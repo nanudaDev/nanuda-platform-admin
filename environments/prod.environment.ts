81a41af23171cb26054228963ac7cad357ed75e5
joseph-nanuda <lsj@widaehan.com>
2020-09-02 10:24:12 +0900

    changed environment variables

--- environments/prod.environment.ts
+++ environments/prod.environment.ts
@@ -2,7 +2,7 @@ import packageInfo from '../package.json';
 import { Environment, EnvironmentType } from './environment.type';
 
 export const ProductionEnvironment: Environment = {
-  baseURL: 'http://61.97.191.100:4200/',
+  baseURL: 'https://platform-api.nanudakitchen.com/',
   NODE_ENV: EnvironmentType.production,
   production: false,
   clientName: packageInfo.name,

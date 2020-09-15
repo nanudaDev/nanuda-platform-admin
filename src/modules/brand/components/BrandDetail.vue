f1b141070640a8feeb791c8ff51f3270fdef64fb
ria-nanuda <kbr@widaehan.com>
2020-09-15 15:08:21 +0900

    브랜드 메인 메뉴 이미지 수정
    메인 메뉴 이미지 수정

--- src/modules/brand/components/BrandDetail.vue
+++ src/modules/brand/components/BrandDetail.vue
@@ -2,7 +2,9 @@
   <section v-if="brandDto">
     <SectionTitle title="브랜드 관리" divider>
       <template v-slot:rightArea>
-        <router-link to="/brand" class="btn btn-secondary">목록으로</router-link>
+        <router-link to="/brand" class="btn btn-secondary"
+          >목록으로</router-link
+        >
       </template>
     </SectionTitle>
     <b-row>
@@ -10,21 +12,50 @@
         <BaseCard title="브랜드 정보">
           <template v-slot:head>
             <div>
-              <b-button variant="danger" v-b-modal.delete_brand>삭제하기</b-button>
-              <b-button variant="primary" v-b-modal.update_brand @click="showUpdateModal()">수정하기</b-button>
+              <b-button variant="danger" v-b-modal.delete_brand
+                >삭제하기</b-button
+              >
+              <b-button
+                variant="primary"
+                v-b-modal.update_brand
+                @click="showUpdateModal()"
+                >수정하기</b-button
+              >
             </div>
           </template>
           <template v-slot:body>
             <div>
-              <div v-if="brandDto.logo && brandDto.logo.length > 0">
-                <div v-for="logo in brandDto.logo" :key="logo.endpoint">
-                  <b-img-lazy
-                    :src="logo.endpoint"
-                    class="rounded mx-auto d-block company-logo"
-                    style="max-height:80px"
-                  />
-                </div>
-              </div>
+              <b-row>
+                <b-col>
+                  <div v-if="brandDto.logo && brandDto.logo.length > 0">
+                    <div v-for="logo in brandDto.logo" :key="logo.endpoint">
+                      <b-img-lazy
+                        :src="logo.endpoint"
+                        class="rounded mx-auto d-block company-logo"
+                        style="max-height:80px"
+                      />
+                    </div>
+                  </div>
+                </b-col>
+                <b-col
+                  v-if="
+                    brandDto.mainMenuImage && brandDto.mainMenuImage.length > 0
+                  "
+                >
+                  <div>
+                    <div
+                      v-for="image in brandDto.mainMenuImage"
+                      :key="image.endpoint"
+                    >
+                      <b-img-lazy
+                        :src="image.endpoint"
+                        class="rounded mx-auto d-block company-logo"
+                        style="max-height:80px"
+                      />
+                    </div>
+                  </div>
+                </b-col>
+              </b-row>
               <ul class="u-list">
                 <li v-if="brandDto.nameKr">
                   브랜드명 : {{ brandDto.nameKr }}
@@ -32,16 +63,17 @@
                 </li>
                 <li v-if="brandDto.category">
                   업종 : {{ brandDto.category.nameKr }}
-                  <span
-                    v-if="brandDto.category.code"
-                  >({{ brandDto.category.code }} )</span>
+                  <span v-if="brandDto.category.code"
+                    >({{ brandDto.category.code }} )</span
+                  >
                 </li>
                 <li v-if="brandDto.desc">설명 : {{ brandDto.desc }}</li>
                 <li v-if="brandDto.showYn">
                   노출 여부 :
                   <b-badge
                     :variant="brandDto.showYn === 'Y' ? 'success' : 'danger'"
-                  >{{ brandDto.showYn }}</b-badge>
+                    >{{ brandDto.showYn }}</b-badge
+                  >
                 </li>
               </ul>
             </div>
@@ -72,10 +104,16 @@
                   <td>{{ menu.no }}</td>
                   <td>{{ menu.nameKr }}</td>
                   <td>
-                    <b-badge :variant="menu.showYn === 'Y' ? 'success' : 'danger'">{{ menu.showYn }}</b-badge>
+                    <b-badge
+                      :variant="menu.showYn === 'Y' ? 'success' : 'danger'"
+                      >{{ menu.showYn }}</b-badge
+                    >
                   </td>
                   <td>
-                    <b-badge :variant="menu.mainYn === 'Y' ? 'success' : 'danger'">{{ menu.mainYn }}</b-badge>
+                    <b-badge
+                      :variant="menu.mainYn === 'Y' ? 'success' : 'danger'"
+                      >{{ menu.mainYn }}</b-badge
+                    >
                   </td>
                   <td>{{ menu.createdAt | dateTransformer }}</td>
                   <td>
@@ -83,7 +121,8 @@
                       variant="primary"
                       v-b-modal.update_menu
                       @click="showMenuUpdateModal(menu.no)"
-                    >수정하기</b-button>
+                      >수정하기</b-button
+                    >
                   </td>
                 </tr>
               </tbody>
@@ -107,9 +146,20 @@
       </b-col>
     </b-row>
     <!-- 브랜드 수정 모달 -->
-    <b-modal id="update_brand" title="브랜드 수정" ok-title="수정" cancel-title="취소" @ok="updateBrand()">
+    <b-modal
+      id="update_brand"
+      title="브랜드 수정"
+      ok-title="수정"
+      cancel-title="취소"
+      @ok="updateBrand()"
+    >
       <b-row no-gutters align-h="end">
-        <b-form-group label="노출 여부" label-size="sm" label-text-align="right" label-cols="8">
+        <b-form-group
+          label="노출 여부"
+          label-size="sm"
+          label-text-align="right"
+          label-cols="8"
+        >
           <b-form-checkbox
             switch
             size="lg"
@@ -119,28 +169,102 @@
           ></b-form-checkbox>
         </b-form-group>
       </b-row>
-      <div v-if="brandDto.logo && brandDto.logo.length > 0 && !logoChanged" class="mb-4">
-        <div v-for="logo in brandDto.logo" :key="logo.endpoint">
-          <b-img-lazy
-            :src="logo.endpoint"
-            class="rounded mx-auto d-block company-logo"
-            style="max-height:80px"
-          />
-        </div>
-      </div>
-      <div v-if="newBrandLogo && newBrandLogo.length > 0 && logoChanged" class="mb-4">
-        <div v-for="logo in newBrandLogo" :key="logo.endpoint">
-          <b-img-lazy
-            :src="logo.endpoint"
-            class="rounded mx-auto d-block company-logo"
-            style="max-height:80px"
-          />
-        </div>
-        <div class="text-center mt-2">
-          <b-button variant="danger" @click="removeBrandLogo()">로고 제거</b-button>
-        </div>
-      </div>
       <b-form-row>
+        <b-col cols="12" md="6" class="mb-3">
+          <label>
+            브랜드 로고
+            <span class="red-text">*</span>
+          </label>
+          <div class="my-2">
+            <div
+              v-if="brandDto.logo && brandDto.logo.length > 0 && !logoChanged"
+              class="mb-4"
+            >
+              <div v-for="logo in brandDto.logo" :key="logo.endpoint">
+                <b-img-lazy
+                  :src="logo.endpoint"
+                  class="rounded mx-auto d-block company-logo"
+                  style="max-height:80px"
+                />
+              </div>
+            </div>
+            <div
+              v-if="newBrandLogo && newBrandLogo.length > 0 && logoChanged"
+              class="mb-4"
+            >
+              <div v-for="logo in newBrandLogo" :key="logo.endpoint">
+                <b-img-lazy
+                  :src="logo.endpoint"
+                  class="rounded mx-auto d-block company-logo"
+                  style="max-height:80px"
+                />
+              </div>
+              <div class="text-center mt-2">
+                <b-button variant="danger" @click="removeBrandLogo()"
+                  >로고 제거</b-button
+                >
+              </div>
+            </div>
+          </div>
+          <div class="custom-file">
+            <b-form-file
+              placeholder="파일 첨부"
+              ref="fileInput"
+              @input="upload($event)"
+            ></b-form-file>
+          </div>
+        </b-col>
+        <b-col cols="12" md="6" class="mb-3">
+          <label>
+            메인 메뉴 이미지
+            <span class="red-text">*</span>
+          </label>
+          <div class="my-2">
+            <div
+              v-if="
+                brandDto.mainMenuImage &&
+                  brandDto.mainMenuImage.length > 0 &&
+                  !logoChanged
+              "
+              class="mb-4"
+            >
+              <div
+                v-for="image in brandDto.mainMenuImage"
+                :key="image.endpoint"
+              >
+                <b-img-lazy
+                  :src="image.endpoint"
+                  class="rounded mx-auto d-block company-logo"
+                  style="max-height:80px"
+                />
+              </div>
+            </div>
+            <div
+              v-if="newMainMenu && newMainMenu.length > 0 && menuChanged"
+              class="mb-4"
+            >
+              <div v-for="image in newMainMenu" :key="image.endpoint">
+                <b-img-lazy
+                  :src="image.endpoint"
+                  class="rounded mx-auto d-block company-logo"
+                  style="max-height:80px"
+                />
+              </div>
+              <div class="text-center mt-2">
+                <b-button variant="danger" @click="removeMainMenu()"
+                  >메뉴 이미지 제거</b-button
+                >
+              </div>
+            </div>
+          </div>
+          <div class="custom-file">
+            <b-form-file
+              placeholder="파일 첨부"
+              ref="fileInputMainMenu"
+              @input="uploadMainmenu($event)"
+            ></b-form-file>
+          </div>
+        </b-col>
         <b-col cols="12" md="6" class="mb-3">
           <label>
             업종 카테고리
@@ -155,17 +279,9 @@
               v-for="category in foodCategorySelect"
               :key="category.code"
               :value="category.no"
-            >{{ category.nameKr }}</option>
+              >{{ category.nameKr }}</option
+            >
           </select>
-        </b-col>
-        <b-col cols="12" md="6" class="mb-3">
-          <label>
-            브랜드 로고
-            <span class="red-text">*</span>
-          </label>
-          <div class="custom-file">
-            <b-form-file placeholder="로고 추가" ref="fileInput" @input="upload($event)"></b-form-file>
-          </div>
         </b-col>
         <b-col cols="12" md="6" class="mb-3">
           <label>
@@ -201,7 +317,8 @@
           <span class="red-text text-center" style="font0-size:11px">
             <i>
               아래 부분은
-              <b>개발자들만이</b> 사용할 수 있으며 잘못 사용하실 경우 운영 사이트에 정확한 정보를 노출 못할 수도 있습니다
+              <b>개발자들만이</b> 사용할 수 있으며 잘못 사용하실 경우 운영
+              사이트에 정확한 정보를 노출 못할 수도 있습니다
             </i>
           </span>
           <b-form-group label-cols="3" label="공간 유형">
@@ -211,7 +328,8 @@
               :key="type"
               :value="type"
               name="space_type_no"
-            >{{ type }}</b-form-radio>
+              >{{ type }}</b-form-radio
+            >
           </b-form-group>
         </b-col>
       </b-form-row>
@@ -276,6 +394,8 @@ export default class BrandDetail extends BaseComponent {
   private foodCategorySelect: FoodCategoryDto[] = [];
   private newBrandLogo: FileAttachmentDto[] = [];
   private logoChanged = false;
+  private newMainMenu: FileAttachmentDto[] = [];
+  private menuChanged = false;
   private showYn: YN[] = [...CONST_YN];
   private spaceType = [2, 1];
   private menus: MenuDto[] = [];
@@ -313,6 +433,12 @@ export default class BrandDetail extends BaseComponent {
       this.brandUpdateDto.logo = this.newBrandLogo;
     } else {
       delete this.brandUpdateDto.logo;
+    }
+
+    if (this.newMainMenu.length > 0) {
+      this.brandUpdateDto.mainMenuImage = this.newMainMenu;
+    } else {
+      delete this.brandUpdateDto.mainMenuImage;
     }
     BrandService.update(this.$route.params.id, this.brandUpdateDto).subscribe(
       res => {
@@ -371,6 +497,30 @@ export default class BrandDetail extends BaseComponent {
   removeBrandLogo() {
     this.$refs['fileInput'].reset();
     this.logoChanged = false;
+  }
+
+  // upload main menu
+  async uploadMainmenu(file: File) {
+    if (file) {
+      const attachments = await FileUploadService.upload(
+        UPLOAD_TYPE.MAIN_MENU_IMAGE,
+        [file],
+      );
+      this.newMainMenu = [];
+      this.newMainMenu.push(
+        ...attachments.filter(
+          fileUpload =>
+            fileUpload.attachmentReasonType === ATTACHMENT_REASON_TYPE.SUCCESS,
+        ),
+      );
+      this.menuChanged = true;
+    }
+  }
+
+  // remove main menu
+  removeMainMenu() {
+    this.$refs['fileInputMainMenu'].reset();
+    this.menuChanged = false;
   }
 
   deleteOne() {

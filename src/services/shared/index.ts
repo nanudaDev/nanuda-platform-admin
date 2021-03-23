8b2a1c23e8de2c277bbdf56a8ea36ccd1e6dbe3c
eddie-nanuda <lsh@widaehan.com>
2021-03-23 17:59:30 +0900

    added new common code types and pickcook consult response dto

--- src/services/shared/index.ts
+++ src/services/shared/index.ts
@@ -2,3 +2,6 @@ export * from './common-code.type';
 export * from './space-type.type';
 export * from './pagination-count.type';
 export * from './analysis/BaeminCategoryCode.type';
+export * from './pickcook-common-code.type';
+export * from './revenue-grade-sentence.type';
+export * from './score-code.type';

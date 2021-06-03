4a99ad4d2b2a73e90d40c44e60d7ad743841677d
Joseph Lee <lsj@widaehan.com>
2021-06-03 16:52:13 +0900

    added consult V3 for admin

--- src/router/modules/pickcook-component.ts
+++ src/router/modules/pickcook-component.ts
@@ -78,6 +78,45 @@ const pickcookComponentRouter: RouteConfig[] = [
       },
     ],
   },
+  {
+    path: '/pickcook/consult-response-v3',
+    name: '상담신청 V3',
+    component: () =>
+      import(
+        '../../modules/pickcook/consult-response-v3/ConsultResponseV3.vue'
+      ),
+    children: [
+      {
+        path: '/pickcook/consult-response-v3',
+        name: 'ConsultResponseV3List',
+        component: () =>
+          import(
+            '../../modules/pickcook/consult-response-v3/components/ConsultResponseV3List.vue'
+          ),
+        meta: {
+          authRequired: true,
+          layout: 'PickcookLayout',
+          roles: [...CONST_ADMIN_USER],
+          title: '상담신청 관리',
+        },
+      },
+      {
+        path: '/pickcook/consult-response-v3/:id([0-9]+)',
+        name: 'ConsultResponseV3Detail',
+        component: () =>
+          import(
+            '../../modules/pickcook/consult-response-v3/components/ConsultResponseV3Detail.vue'
+          ),
+        meta: {
+          authRequired: true,
+          layout: 'PickcookLayout',
+          roles: [...CONST_ADMIN_USER],
+          detailPage: true,
+          title: '상담신청',
+        },
+      },
+    ],
+  },
 ];
 
 export default pickcookComponentRouter;

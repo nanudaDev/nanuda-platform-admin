2f7731701e112226266501f8f9c3c9f571b1f991
ria-nanuda <kbr@widaehan.com>
2021-08-11 19:10:56 +0900

    상권분석 리포트 리뉴얼 인쇄 영영 페이지 테스트

--- src/modules/pickcook/consult-report/components/ConsultReportPrint.vue
+++ src/modules/pickcook/consult-report/components/ConsultReportPrint.vue
@@ -1,19 +1,51 @@
 <template>
   <div id="report">
+    <div class="text-center mb-4 d-print-none">
+      <b-btn
+        variant="dark"
+        size="lg"
+        @click="generateReport()"
+        class="d-print-none txt-white"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          class="bi bi-printer"
+          viewBox="0 0 16 16"
+        >
+          <path d="M2.5 8a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1z" />
+          <path
+            d="M5 1a2 2 0 0 0-2 2v2H2a2 2 0 0 0-2 2v3a2 2 0 0 0 2 2h1v1a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2v-1h1a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2h-1V3a2 2 0 0 0-2-2H5zM4 3a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1v2H4V3zm1 5a2 2 0 0 0-2 2v1H2a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-1v-1a2 2 0 0 0-2-2H5zm7 2v3a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-3a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1z"
+          />
+        </svg>
+        <span class="ml-2">프린트</span>
+      </b-btn>
+    </div>
     <div class="page cover">
       <div class="page-inner">
         <div>
           <header>
             <h1>
+              <span
+                ><img
+                  src="https://kr.object.ncloudstorage.com/common-storage-pickcook/common/logo_symbol_w.png"
+                  alt="픽쿡"
+                  class=""
+              /></span>
               <span>PICKCOOK</span>
               상권분석 리포트
             </h1>
-            <address contenteditable>
-              <p>RIA</p>
-              <p>101 E. Chapman Ave<br />Orange, CA 92866</p>
-              <p>010 4153 7907</p>
-            </address>
+            <p class="mt-4">
+              <span class="sub-title-en">ANALYSIS REPORT</span>
+            </p>
           </header>
+          <address contenteditable>
+            <p>RIA</p>
+            <p>101 E. Chapman Ave<br />Orange, CA 92866</p>
+            <p>010 4153 7907</p>
+          </address>
         </div>
       </div>
     </div>
@@ -21,43 +53,56 @@
       <div class="page-inner">
         <div>
           <header>
-            <h1>상권개요</h1>
+            <h1>01 상권개요</h1>
           </header>
           <article>
-            <table class="table">
-              <colgroup>
-                <col width="25%" />
-                <col width="*" />
-                <col width="25%" />
-                <col width="*%" />
-              </colgroup>
-              <tbody>
-                <tr>
-                  <th scope="row">음식점수(개)</th>
-                  <td>10</td>
-                  <th scope="row">주거인구(명)</th>
-                  <td>10</td>
-                </tr>
-                <tr>
-                  <th scope="row">총세대수(세대)</th>
-                  <td>10</td>
-                  <th scope="row">직장인구(명)</th>
-                  <td>10</td>
-                </tr>
-                <tr>
-                  <th scope="row">평균 영업기간(년)</th>
-                  <td>10</td>
-                  <th scope="row">유동인구(명)</th>
-                  <td>10</td>
-                </tr>
-                <tr>
-                  <th scope="row">주요 연령대</th>
-                  <td>10</td>
-                  <th scope="row"></th>
-                  <td></td>
-                </tr>
-              </tbody>
-            </table>
+            <section>
+              <div id="map" style="height:300px"></div>
+            </section>
+            <section>
+              <table class="table">
+                <colgroup>
+                  <col width="25%" />
+                  <col width="*" />
+                  <col width="25%" />
+                  <col width="*%" />
+                </colgroup>
+                <tbody>
+                  <tr>
+                    <th scope="row">음식점수(개)</th>
+                    <td>10</td>
+                    <th scope="row">주거인구(명)</th>
+                    <td>10</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">총세대수(세대)</th>
+                    <td>10</td>
+                    <th scope="row">직장인구(명)</th>
+                    <td>10</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">평균 영업기간(년)</th>
+                    <td>10</td>
+                    <th scope="row">유동인구(명)</th>
+                    <td>10</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">주요 연령대</th>
+                    <td>10</td>
+                    <th scope="row"></th>
+                    <td></td>
+                  </tr>
+                </tbody>
+              </table>
+            </section>
+            <section>
+              <apexchart
+                width="500"
+                type="donut"
+                :options="chartOptions"
+                :series="series"
+              ></apexchart>
+            </section>
           </article>
         </div>
         <footer>
@@ -69,7 +114,7 @@
       <div class="page-inner">
         <div>
           <header>
-            <h1>매출분석</h1>
+            <h1>02 매출분석</h1>
           </header>
           <article>
             <section>
@@ -178,18 +223,38 @@ import ConsultResponseV3Service from '@/services/pickcook/consult-response-v3.se
 import { ConsultResponseV3Dto, SalesRequestDto, SalesResponseDto } from '@/dto';
 import RevenueChart from '../add-on/RevenueChart.vue';
 import DoughnutChart from '../add-on/DoughnutChart.vue';
+import VueApexCharts from 'vue-apexcharts';
 
 @Component({
   name: 'ConsultReportPrint',
   components: {
     RevenueChart,
     DoughnutChart,
+    VueApexCharts,
   },
 })
 export default class ConsultReportPrint extends BaseComponent {
+  declare $refs: any;
+
+  generateReport() {
+    window.print();
+  }
+
+  private chartOptions = {
+    chart: {
+      id: 'vuechart-example',
+    },
+    // xaxis: {
+    //   categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998],
+    // },
+  };
+  private series = [44, 55, 13, 33];
+
   private salesRequestDto = new SalesRequestDto();
   private salesResponseDto: any = new SalesResponseDto();
   private consultResponseV3Dto = new ConsultResponseV3Dto();
+  private map;
+  private reportAddress = '성남시 수정구 신흥2동';
 
   // 상권매출현황 차트
   private revenueData = [];
@@ -285,14 +350,76 @@ export default class ConsultReportPrint extends BaseComponent {
     );
   }
 
+  // 지도 가져오기
+  setMap() {
+    const geocoder = new window.kakao.maps.services.Geocoder();
+
+    const callback = function(result, status) {
+      if (status === window.kakao.maps.services.Status.OK) {
+        const mapContainer = document.getElementById('map');
+        const mapOption = {
+          center: new window.kakao.maps.LatLng(result[0].y, result[0].x),
+          level: 5,
+          minLevel: 3,
+        };
+
+        const map = new window.kakao.maps.Map(mapContainer, mapOption);
+
+        const content = ``;
+        const markerPosition = new window.kakao.maps.LatLng(
+          result[0].y,
+          result[0].x,
+        );
+
+        const customOverlay = new window.kakao.maps.CustomOverlay({
+          position: markerPosition,
+          content: content,
+          // image: markerImage,
+        });
+
+        const circle = new window.kakao.maps.Circle({
+          map: map,
+          center: new window.kakao.maps.LatLng(result[0].y, result[0].x),
+          strokeWeight: 2,
+          strokeColor: '#477DE6',
+          strokeOpacity: 1,
+          strokeStyle: 'solid',
+          fillColor: '#4CB0F8',
+          fillOpacity: 0.3,
+        });
+        circle.setRadius(800);
+        circle.setMap(map);
+        customOverlay.setMap(map);
+      }
+    };
+
+    geocoder.addressSearch(this.reportAddress, callback);
+  }
+
+  setAddress(res) {
+    this.reportAddress = `${res.sido} ${res.sigungu} ${res.bname}`;
+    const geocoder = new window.kakao.maps.services.Geocoder();
+    const callback = (results, status) => {
+      if (status === window.kakao.maps.services.Status.OK) {
+        if (results[0]) {
+          this.salesRequestDto.hdongCode = +results[0].address.h_code;
+        }
+      } else {
+        // toast.error('다른 주소로 선택해주세요');
+      }
+    };
+    geocoder.addressSearch(res.address, callback);
+    this.$bvModal.hide('postcode');
+  }
+
   created() {
     this.getConsultData(this.$route.params.id);
+    this.setMap();
   }
 }
 </script>
 <style lang="scss">
 /* reset */
-
 .chart-container {
   max-width: 100%;
   canvas {
@@ -334,11 +461,16 @@ export default class ConsultReportPrint extends BaseComponent {
 
 /* heading */
 
-h1 {
-  font: bold 100% sans-serif;
-  letter-spacing: 0.5em;
-  text-align: center;
-  text-transform: uppercase;
+header {
+  text-align: left;
+  border-bottom: 2px solid #004d8a;
+  color: #004d8a;
+  padding-bottom: 1em;
+  margin-bottom: 1em;
+  h1 {
+    font-weight: bold;
+    font-size: 18pt;
+  }
 }
 
 /* table */
@@ -389,23 +521,15 @@ body {
   min-width: 0 !important;
 }
 
-section {
-  + section {
-    margin-top: 1em;
-  }
-}
-
 /* page */
 .page {
-  box-sizing: border-box;
   margin: 0 auto;
   overflow: hidden;
-  width: 8.5in;
+  width: 8.3in;
   height: 11.7in;
   background: #fff;
-  border-radius: 1px;
   box-shadow: 0 0 1in -0.25in rgba(0, 0, 0, 0.5);
-
+  padding: 0.5in;
   + .page {
     margin-top: 1in;
   }
@@ -415,8 +539,7 @@ section {
     // display: flex;
     // flex-direction: column;
     //  justify-content: space-between;
-    height: 10.7in;
-    margin: 0.5in;
+    height: 10.68in;
     overflow: hidden;
   }
 
@@ -425,12 +548,26 @@ section {
 
     header {
       margin-top: 3in;
+      color: #fff;
+      text-align: center;
       h1 {
         font-size: 24pt;
         background: transparent;
+        letter-spacing: 0.5em;
+        padding-left: 0.5em;
         span {
           display: block;
         }
+      }
+      .sub-title-en {
+        display: inline-block;
+        text-align: center;
+        padding-top: 1em;
+        font-size: 12px;
+        border-top: 1px solid rgba(255, 255, 255, 0.5);
+        color: #fff;
+        letter-spacing: 2em;
+        padding-left: 2em;
       }
     }
     .page-inner {
@@ -440,34 +577,14 @@ section {
       position: absolute;
       right: 0.25in;
       bottom: 0.25in;
-      font-size: 14pt;
+      font-size: 18pt;
+      color: #fff;
+      text-align: right;
     }
   }
 }
 
-/* header */
-
-header:after {
-  clear: both;
-  content: '';
-  display: table;
-}
-
-header h1 {
-  background: #000;
-  border-radius: 0.25em;
-  color: #fff;
-  margin: 0 0 1em;
-  padding: 0.5em 0;
-}
-header address {
-  float: left;
-  font-size: 75%;
-  font-style: normal;
-  line-height: 1.25;
-  margin: 0 1em 1em 0;
-  color: #fff;
-}
+/* footer */
 footer {
   text-align: center;
   font-size: 8pt;
@@ -490,6 +607,12 @@ article address {
   font-weight: bold;
 }
 
+section {
+  + section {
+    margin-top: 3em;
+  }
+}
+
 @media print {
   * {
     -webkit-print-color-adjust: exact;
@@ -508,14 +631,13 @@ article address {
   .page {
     box-shadow: none;
     width: auto;
-    height: 11.7in;
-    min-height: 1058px;
+    height: auto;
     margin: 0;
     + .page {
       margin-top: 0;
     }
     .page-inner {
-      height: 10.7in;
+      height: 10.68in;
     }
   }
 
